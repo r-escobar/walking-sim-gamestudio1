f139f31c601d2d0ef2478ad3b086cf21f5e22af2
TATammy <burningtammy@gmail.com>
2018-10-12 13:51:26 -0400

    Changed Ripple to display when head bob down

---
+++ GameStudio1_walkingSim/Assets/Scripts/FootstepParticleControllerTammy.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootstepParticleControllerTammy : MonoBehaviour
+{
+
+	public ParticleSystem partSys;
+
+	public CharacterController charController;
+
+	private float timer = 0f;
+
+	private float HeadBobDownTime = 0f;
+
+	void Start()
+	{
+		var HeadBobScript = GetComponentInChildren<HeadBob>();
+		var HeadbobSpeed = HeadBobScript.bobbingSpeed;
+		HeadBobDownTime = 1.5f * Mathf.PI / (100f * HeadbobSpeed);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (charController.velocity.magnitude != 0f)
+		{
+			timer += Time.deltaTime;
+			if (timer >= HeadBobDownTime)
+			{
+				timer = 0f;
+				partSys.transform.position = new Vector3(transform.position.x, 0.001f, transform.position.z);
+				partSys.Play();
+				
+			}
+		}
+		else
+		{
+			timer = 0f;
+		}
+	}
+}
