c645e93c8bf02624a0b198ab2b5732735067aaca
r-escobar <rescobar713@gmail.com>
2018-11-04 03:56:41 -0500

    Added controller scripts for lots of props

---
+++ GameStudio1_walkingSim/Assets/Scripts/PianoAudioController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PianoAudioController : MonoBehaviour
+{
+	public AudioSource audSrc;
+
+	private bool hasPerformedAction = false;
+	
+
+	public void PerformUndeformAction()
+	{
+		if(hasPerformedAction)
+			return;
+		hasPerformedAction = true;
+		audSrc.Play();
+	}
+}
