7b7be16e6f6284cc19fb4177a52e29d63647e662
r-escobar <rescobar713@gmail.com>
2018-11-06 03:56:37 -0500

    Working on hospital area

---
+++ GameStudio1_walkingSim/Assets/Scripts/HospitalBedController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HospitalBedController : MonoBehaviour {
+	public AudioSource audSrc;
+
+	private bool hasPerformedAction = false;
+	
+
+	public void PerformUndeformAction()
+	{
+		if(hasPerformedAction)
+			return;
+		hasPerformedAction = true;
+		audSrc.Play();
+	}
+}
